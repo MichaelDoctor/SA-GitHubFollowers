773f543f0f6d2409d037f490677fadf1f83da4e2
Michael Doctor <michaelcdoctor@gmail.com>
2021-06-04 18:19:04 -0600

    Changed Main Interface to programatically display tabbar > nav controllers > view controllers, Added assets

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Michael Doctor on 2021-06-04.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- GitHub Followers/SearchVC.swift
+++ GitHub Followers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GitHub Followers
 //
 //  Created by Michael Doctor on 2021-06-04.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemGreen
     }
 
 
 }
-
